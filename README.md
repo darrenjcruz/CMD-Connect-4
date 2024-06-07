bc851c0093bc4c658f5527ca5f229e9e168d2322
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2024-06-06 21:46:27 -0700

    Created new GitHub Repo for Connect 4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CMD-Connect-4
+
+a working Connect 4 game made with Python. The game is played through the Terminal. 
---
+++ connectfourconsole.py
@@ -0,0 +1,8 @@
+import connectfour
+
+def main():
+    game = connectfour.ConnectFour()
+    game.menu()
+
+if __name__ == '__main__':
+    main()
